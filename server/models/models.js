48326a6b8ae3994d92a838065e53f3b423132f01
pinkpagejpeg <isip_a.a.stolbova@mpt.ru>
2024-02-20 18:49:40 +0300

    feat: add models

---
+++ server/models/models.js
@@ -0,0 +1,53 @@
+const sequelized = require('../db')
+const {DataTypes} = require('sequelize')
+
+const User = sequelized.define('user', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    email: {type: DataTypes.STRING, unique: true},
+    password: {type: DataTypes.STRING},
+    role: {type: DataTypes.STRING, defaultValue: "USER"},
+})
+
+const Task = sequelized.define('task', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    info: {type: DataTypes.STRING},
+    status: {type: DataTypes.BOOLEAN, defaultValue: false},
+})
+
+const Goal = sequelized.define('goal', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    info: {type: DataTypes.STRING},
+})
+
+const Goal_item = sequelized.define('goal_item', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    info: {type: DataTypes.STRING},
+    status: {type: DataTypes.BOOLEAN, defaultValue: false},
+})
+
+const Feedback = sequelized.define('feedback', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    info: {type: DataTypes.STRING},
+    date: {type: DataTypes.DATE},
+    status: {type: DataTypes.BOOLEAN, defaultValue: false},
+})
+
+User.hasMany(Goal)
+Goal.belongsTo(User)
+
+User.hasMany(Task)
+Task.belongsTo(User)
+
+User.hasMany(Feedback)
+Feedback.belongsTo(User)
+
+Goal.hasMany(Goal_item)
+Goal_item.belongsTo(Goal)
+
+module.exports = {
+    User,
+    Task,
+    Goal,
+    Goal_item,
+    Feedback
+}
