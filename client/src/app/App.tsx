9ffe597908dd3905ae63c78a3c8c06bf53e72eb7
pinkpagejpeg <pinkpagejpeg@gmail.com>
2024-10-20 17:38:31 +0300

    fix: FSD architecture implement

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,9 +1,33 @@
-import { FC } from 'react'
+import { useState, useEffect, FC } from 'react'
 import { AppRouter } from './routers'
 import './styles/App.scss'
+import { check, getUser } from '../shared/api'
 import { MainProvider } from './providers'
+import { useAppDispatch, useTypedSelector } from '../features/hooks'
+import { setAuthTrue } from '../entities/users'
 
 const App: FC = () => {
+  const { user, isAuth } = useTypedSelector((state) => state.user)
+  const [loading, setLoading] = useState(true)
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    check().then((data) => {
+      dispatch(setAuthTrue(data))
+      fetchUser()
+    }).finally(() => setLoading(false))
+  }, [])
+
+  const fetchUser = async () => {
+    try {
+      if (user.id) {
+        const data = await getUser(user.id)
+        dispatch(setAuthTrue(data))
+      }
+    } catch (e) {
+      alert(`Ошибка при получении информации о пользователе: ${e.response.data.message}`)
+    }
+  }
 
   return (
     <div className='App'>
