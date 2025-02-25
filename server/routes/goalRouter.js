c8c33fe32a858cf231118dba7cad6c0618cf26a4
pinkpagejpeg <pinkpagejpeg@gmail.com>
2025-02-25 18:02:24 +0300

    fix: goalRouter divided into goalRouter and goalItemRouter

--- server/routes/goalRouter.js
+++ server/routes/goalRouter.js
@@ -6,41 +6,6 @@ const { check } = require('express-validator')
 const router = new Router()
 router.use(AuthMiddleware)
 
-// Subgoals
-// Add subgoal (authorized users)
-router.post(
-    '/:goalId/items',
-    check('info', 'Подцель не введена').notEmpty(),
-    goalController.createItem
-)
-
-// Update subgoal status (authorized users)
-router.patch(
-    '/:goalId/items/:goalItemId/status', 
-    check('status', 'Отсутствует статус подцели').exists(),
-    goalController.changeItemStatus
-)
-
-// Update subgoal (authorized users)
-router.patch(
-    '/:goalId/items/:goalItemId',
-    check('info', 'Подцель не введена').notEmpty(),
-    goalController.updateItem
-)
-
-// Delete subgoal (authorized users)
-router.delete(
-    '/:goalId/items/:goalItemId', 
-    goalController.deleteItem
-)
-
-// Get subgoals by goal id (authorized users)
-router.get(
-    '/:goalId/items', 
-    goalController.getAllItems
-)
-
-// Goal
 // Get goal progress (authorized users)
 router.get(
     '/:goalId/progress',
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,15 @@
 const Router = require('express')
-const router = new Router()
 const userRouter = require('./userRouter')
 const taskRouter = require('./taskRouter')
+const goalItemRouter = require('./goalItemRouter')
 const goalRouter = require('./goalRouter')
 const feedbackRouter = require('./feedbackRouter')
 
+const router = new Router()
+
 router.use('/user', userRouter)
 router.use('/task', taskRouter)
+router.use('/goal', goalItemRouter)
 router.use('/goal', goalRouter)
 router.use('/feedback', feedbackRouter)
 
