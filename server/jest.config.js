f49b78d1a6f2a05c4332ef3cfbbf04e7b82dd37b
pinkpagejpeg <pinkpagejpeg@gmail.com>
2025-03-04 16:19:25 +0300

    feat: add unit tests for add task in taskRouter

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
     testEnvironment: 'node',
     testMatch: ['**/__tests__/**/*.test.js'],
-    testPathIgnorePatterns: ['<rootDir>/routes/__tests__/integration/checkRouter.js'],
+    testPathIgnorePatterns: [
+        '<rootDir>/routes/__tests__/integration/checkRouter.js',
+        '<rootDir>/routes/__tests__/unit/checkRouter.js',
+    ],
     setupFilesAfterEnv: ['<rootDir>/jest.setup.js']
 }
